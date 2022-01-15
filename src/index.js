b99e1c9857b584a7abba787bc56640f6d5679a65
Aleksandr <dentranscom@gmail.com>
2022-01-15 16:41:21 +0200

    feat: shorten the code

--- src/index.js
+++ src/index.js
@@ -5,62 +5,22 @@ module.exports = function toReadable (number) {
     if (number < 20) {
         return units[number];
     }
-    if (number === 20) {
-        return 'twenty';
-    }
-    if (number === 30) {
-        return 'thirty';
-    }
-    if (number === 40) {
-        return 'forty';
-    }
-    if (number === 50) {
-        return 'fifty';
-    }
-    if (number === 60) {
-        return 'sixty';
-    }
-    if (number === 70) {
-        return 'seventy';
-    }
-    if (number === 80) {
-        return 'eighty';
-    }
-    if (number === 90) {
-        return 'ninety';
-    }
+    
     if (number < 100) {
         digit = number % 10;
-        return tens[Math.floor(number / 10) - 2] + " " + (digit > 0 ? units[digit] : "");
+        if (digit == 0) {
+            return tens[Math.floor(number / 10) - 2];
+        } else {
+            return tens[Math.floor(number / 10) - 2] + " " + (digit > 0 ? units[digit] : "");
+        }
     }
-    if (number === 100) {
-        return 'one hundred';
-    }
-    if (number === 200) {
-        return 'two hundred';
-    }
-    if (number === 300) {
-        return 'three hundred';
-    }
-    if (number === 400) {
-        return 'four hundred';
-    }
-    if (number === 500) {
-        return 'five hundred';
-    }
-    if (number === 600) {
-        return 'six hundred';
-    }
-    if (number === 700) {
-        return 'seven hundred';
-    }
-    if (number === 800) {
-        return 'eight hundred';
-    }
-    if (number === 900) {
-        return 'nine hundred';
-    }
+
     if (number < 1000) {
-        return units[Math.floor(number / 100)] + " hundred " + (number % 100 > 0 ? toReadable(number % 100) : "");
+        let rem = number % 100;
+        if (rem == 0) {
+            return units[Math.floor(number / 100)] + " hundred";
+        } else {
+            return units[Math.floor(number / 100)] + " hundred " + (number % 100 > 0 ? toReadable(number % 100) : "").trim();
+        }
     }
 };
